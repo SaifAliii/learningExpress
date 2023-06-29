d6bfc2085a0a14d2d2b22e8ab0acb798bde25bf4
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-06-29 16:24:44 +0500

    making of a simple BMI calculator

--- BMI/BMI.js
+++ BMI/BMI.js
@@ -9,8 +9,8 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var height = Number(req.body.height);
-  var weight = Number(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var weight = parseFloat(req.body.weight);
   var result = weight / (height * height);
   res.send("The BMI is: " + result);
 });
